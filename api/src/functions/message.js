8694284847e6b64b9c2950c3556784d50f21cca1
Elliott Griffiths <99219139+Elliott-Griffiths@users.noreply.github.com>
2023-11-02 16:20:22 +0000

    Updated the repository with my changes

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -1,5 +1,13 @@
-module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
-};
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (context, req) => {
+        context.log(`Http function processed request for url "${req.url}"`);
+
+        const name = req.query.name || req.body || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
