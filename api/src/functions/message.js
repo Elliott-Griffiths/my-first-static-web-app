5fa1fb89730392330b7f0f577b75fae92f76a61f
Elliott Griffiths <99219139+Elliott-Griffiths@users.noreply.github.com>
2023-11-02 17:29:07 +0000

    Updated the repository with my changes

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -4,7 +4,7 @@ app.http('message', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (context, req) => {
-        context.log(`Http function processed request for url "${req.url}"`);
+        // context.log(`Http function processed request for url "${req.url}"`);
 
         const name = req.query.name || req.body || 'world';
 
